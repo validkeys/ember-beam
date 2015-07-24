0eaea9626cb6fdd338e32af9a5a804fa237abfdb
Kyle Davis <validkeys@gmail.com>
2015-07-23 22:23:05 -0400

    Added a few more tests

--- tests/unit/services/beam-test.js
+++ tests/unit/services/beam-test.js
@@ -1,6 +1,7 @@
 import Ember from 'ember';
-// import sinon from 'sinon';
+import sinon from 'sinon';
 import { moduleFor, test } from 'ember-qunit';
+import BaseAdapter from 'ember-beam/beams/adapters/base';
 
 let registry, container,
     appConfig = {
@@ -52,6 +53,32 @@ test('after initializing, the _config object should have the beamConfig and a co
   assert.ok(config.get("__container__"));
   assert.ok(config.get("content.mixpanel"), 'has mixpanel');
   assert.equal(config.get('content.mixpanel'), "12345");
-
 });
 
+test('I should be able to track an event', function(assert){
+  var spy = sinon.spy();
+  var testerAdapter = BaseAdapter.extend({
+    emit: spy
+  });
+  registry.register("beam:adapters/tester", testerAdapter);
+  register({ beam: { tester: "12345" } });
+
+  var service = this.subject({ container: container });
+  service.push("testEvent", { test: true }, {});
+
+  assert.ok(spy.calledOnce, 'The emit method was called');
+  assert.ok(spy.calledWith("testEvent", { test: true }));
+});
+
+// If the adapter has lowercase keys, I should key back my object with all lowercase keys
+// If the adapter has uppercase keys, I should key back my object with all uppercase keys
+// If the adapter has capitalized keys, I should key back my object with all capitalized keys
+// If the adapter has camelCased keys, I should key back my object with all camelCased keys
+// If the adapter is set to flattenPayload, the payload should be flattened
+// Test ^ with two adapters to ensure no collisions
+
+// If the adapter has a transform, it should be applied
+// Test ^ with two adapters to ensure no collisions
+
+// if the adapter has hooks they should be run
+// Test ^ with two adapters to ensure no collisions
